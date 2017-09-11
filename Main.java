5457c85386526735678d23f538425c5afb7c4f89
sruthi2504 <31837575+sruthi2504@users.noreply.github.com>
2017-09-11 13:48:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+    StudentGroup getStudent=new StudentGroup(30);
+    Date d=new Date(1997,4,25)
+    Student s1=new Student(40,"abc",d89.0)
+    system(sg.setstudent(0).getid());
+
+
+}
